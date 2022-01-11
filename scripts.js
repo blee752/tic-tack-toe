1f6372492084517f2f56f0f4541b3866a4ee2a57
Brian <Blee752@mtroyal.ca>
2022-01-11 16:03:38 -0700

    updates display to indicate whos turn it is and who won or if a tie has occured

--- scripts.js
+++ scripts.js
@@ -3,6 +3,32 @@
 
 (function () {
 
+
+    window.onload = event => {
+        const startBt = document.querySelector('.submitBt');
+        
+        startBt.addEventListener('click', event => {
+
+            console.log(document.getElementById('p1-name').value);
+            let p1Name = document.getElementById('p1-name').value;
+            let p2Name = document.getElementById('p2-name').value;
+
+            if (p1Name === '') {
+                p1Name = 'Player 1';
+            }
+            if (p2Name === ''){
+                p2Name = 'Player 2';
+            }
+
+
+            document.querySelector('.page1').style.display = 'none';
+            document.querySelector('.page2').style.display = 'block';
+
+            displayController.initGame(p1Name, p2Name);
+
+        })
+    }
+
     //need to remake this the board. Page needs to have the board rendered from this
     const gameObject = (() => {
         const gameBoard = ['', '', '', '', '', '', '', '', '']
@@ -14,12 +40,12 @@
                 tile.innerText = gameBoard[i];
                 tile.dataset.position = i;
 
-                if(gameBoard[i] !== ''){
+                if (gameBoard[i] !== '') {
                     tile.dataset.filled = true;
                 }
                 else tile.dataset.filled = false;
 
-                
+
                 tile.className = 'boardTile';
                 if (i === 0 || i === 6 || i === 3) {
                     tile.classList.add('right');
@@ -50,14 +76,35 @@
             for (i = 0; i < gameBoard.length; i++) {
                 gameBoard[i] = '';
             }
-            render();
+            const board = document.querySelector('.grid-container');
+            board.replaceChildren();
         }
         const setBoard = (index, marker) => {
             gameBoard[index] = marker;
             console.log(gameBoard);
             reset();
         }
-        return { render, setBoard, reset, restartGame, getBoard }
+
+        const winCheck = (currPlayer) => {
+            const marker = currPlayer.getSymbol();
+            //checks all rows for a win
+            if ((gameBoard[0] === marker && gameBoard[1] === marker && gameBoard[2] === marker) || (gameBoard[3] === marker && gameBoard[4] === marker && gameBoard[5] === marker) || (gameBoard[6] === marker && gameBoard[7] === marker && gameBoard[8] === marker)) {
+                console.log(`${currPlayer.getName()} is the winner with a straight row!`);
+                return 'true';
+            }
+            //checks all columns for a win
+            else if ((gameBoard[0] === marker && gameBoard[3] === marker && gameBoard[6] === marker) || (gameBoard[1] === marker && gameBoard[4] === marker && gameBoard[7] === marker) || (gameBoard[2] === marker && gameBoard[5] === marker && gameBoard[8] === marker)) {
+                console.log(`${currPlayer.getName()} is the winner with a straight column!`);
+                return 'true'
+            }
+            //checks diagonals for a win
+            else if ((gameBoard[0] === marker && gameBoard[4] === marker && gameBoard[8] === marker) || (gameBoard[2] === marker && gameBoard[4] === marker && gameBoard[6] === marker)) {
+                console.log(`${currPlayer.getName()} is the winner with a diagonal!`);
+                return 'true';
+            }
+            else return 'false';
+        }
+        return { render, setBoard, reset, restartGame, getBoard, winCheck }
     })();
 
     /* game logic/steps?
@@ -91,7 +138,9 @@
             turnsTaken++;
         }
 
-
+        const resetTurnsTaken = () => {
+            turnsTaken = 0;
+        }
         const getCurr = () => currTurn;
         const setCurr = () => {
             currTurn = !currTurn;
@@ -101,9 +150,12 @@
         const getStatus = () => winner;
 
         const getName = () => userName;
+        const setName = (name) => {
+            userName = name;
+        }
 
 
-        return { getSymbol, getTurn, incrementTurn, setWinner, getStatus, getCurr, setCurr, getName};
+        return { getSymbol, getTurn, incrementTurn, setWinner, getStatus, getCurr, setCurr, getName, resetTurnsTaken, setName };
     };
 
     //redo this as the game controller
@@ -111,72 +163,103 @@
         //what does this object need to control?
         //pointers to the game board
         //players
+        const resetBt = document.querySelector('.reset')
+        const board = document.querySelector('.grid-container');
+        const turnindicator = document.querySelector('.turn-indicator');
+        let currPlayer;
+        const player1 = Player('p1', 'x');
+        const player2 = Player('p2', 'o');
+        let check = 'false';
+        let winner;
+        player1.setCurr();
+        currPlayer = player1;
 
-        const winCheck = (currPlayer) => {
-            const marker = currPlayer.getSymbol();
-            const gameBoard = gameObject.getBoard();
-            //checks all rows for a win
-            if((gameBoard[0]=== marker && gameBoard[1]===marker && gameBoard[2]===marker) || (gameBoard[3]=== marker && gameBoard[4]===marker && gameBoard[5]===marker) || (gameBoard[6]=== marker && gameBoard[7]===marker && gameBoard[8]===marker)){
-                console.log(`${currPlayer.getName()} is the winner with a straight row!`);
+
+
+        const reset = () => {
+            currPlayer;
+            check = 'false';
+            winner = false;
+
+            player1.resetTurnsTaken();
+            player2.resetTurnsTaken();
+
+            if (player1.getCurr() === true) {
+                //do nothing if player1 is already true;
             }
-            //checks all columns for a win
-            else if ((gameBoard[0]=== marker && gameBoard[3]===marker && gameBoard[6]===marker) || (gameBoard[1]=== marker && gameBoard[4]===marker && gameBoard[7]===marker) || (gameBoard[2]=== marker && gameBoard[5]===marker && gameBoard[8]===marker)) {
-                console.log(`${currPlayer.getName()} is the winner with a straight column!`);
+            else {
+                player1.setCurr(); //change false to true for next game
             }
-            //checks diagonals for a win
-            else if ((gameBoard[0]=== marker && gameBoard[4]===marker && gameBoard[8]===marker) || (gameBoard[2]=== marker && gameBoard[4]===marker && gameBoard[6]===marker)) {
-                console.log(`${currPlayer.getName()} is the winner with a diagonal!`);
+            currPlayer = player1;
+            if (player2.getCurr() === true) {
+                player2.setCurr(); //reverse player 2 to be second
             }
+            gameObject.restartGame();
+            initGame(player1.getName(), player2.getName());
+            console.log('reset worked?');
         }
 
-        /* what needs to be there for the game to work?
-                listen for button on click to set player marker
-                know which player's turn it is currently
-                check for win or tie */
-        const initGame = () => {
+        const initGame = (p1Name,p2Name) => {
             gameObject.render();
-            const board = document.querySelector('.grid-container');
-            let currPlayer;
-            const player1 = Player('p1', 'x');
-            const player2 = Player('p2', 'o');
-
-            player1.setCurr();
-            currPlayer = player1;
-            board.addEventListener('click', event => {
-                if (event.target.className.includes('boardTile') && event.target.dataset.filled !== 'true') {
-                    gameObject.setBoard(event.target.dataset.position, currPlayer.getSymbol());
-                    console.log(`Turn before update ${currPlayer.getTurn()}`)
-                    currPlayer.incrementTurn();
+            player1.setName(p1Name);
+            player2.setName(p2Name);
+            turnindicator.innerText = `${currPlayer.getName()}'s turn`
+            console.log(currPlayer.getName());
+            board.addEventListener('click', playRound);
+            resetBt.addEventListener('click', reset)
+        }
 
 
-                    if(currPlayer.getTurn() >= 3){
-                        winCheck(currPlayer);
-                    }
 
-                    if (currPlayer === player1) {
-                        currPlayer = changeTurn(currPlayer, player2);
-                    }
-                    else currPlayer = changeTurn(currPlayer, player1);
+        const playRound = (event) => {
+            
+            console.log(currPlayer.getName());
+            if (event.target.className.includes('boardTile') && event.target.dataset.filled !== 'true') {
+                gameObject.setBoard(event.target.dataset.position, currPlayer.getSymbol());
+                console.log(`Turn before update ${currPlayer.getTurn()}`)
+                currPlayer.incrementTurn();
+
+
+                if (currPlayer.getTurn() >= 3) {
+                    check = gameObject.winCheck(currPlayer);
 
                 }
+                if (player1.getTurn() === 5 && player2.getTurn() === 4 && check !== 'true') {
+                    //declare tie
+                    check = 'tie';
+                    console.log('tie has occured');
+                    turnindicator.innerText = `Tie!`
+                    return;
+                }
 
-            })
+                if (check === 'true') {
+                    board.removeEventListener('click', playRound);
+                    console.log('win has occured')
+                    winner = currPlayer.getName();
+                    turnindicator.innerText = `${winner} has won the game!`
+                    return;
+                }
+                if (currPlayer === player1) {
+                    currPlayer = changeTurn(player2);
+                }
+                else currPlayer = changeTurn(player1);
 
+            }
         }
 
-        const changeTurn = (currPlayer, nextPlayer) => {
+        const changeTurn = (nextPlayer) => {
             currPlayer.setCurr();
             currPlayer = nextPlayer;
             currPlayer.setCurr();
+            turnindicator.innerText = `${currPlayer.getName()}'s turn`
             return currPlayer;
         }
 
-
-
-        return { initGame }
+        return { initGame, reset }
     })();
 
-    displayController.initGame();
+
+
 })();
 
 
